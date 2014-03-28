d440d991c6f48fcc1223b0fe486ad7781736ae08
Anders S. Christensen <andersx@nano.ku.dk>
2014-03-28 16:42:07 +0100

    Updated visualize_noe script with proper reporting of number of NOE constraints.

--- scripts/visualize_noe.py
+++ scripts/visualize_noe.py
@@ -1,6 +1,10 @@
 import glob
 import sys
 import string
+import Bio.PDB
+
+
+
 
 
 def parse_noe(filename):
@@ -17,9 +21,34 @@ def parse_noe(filename):
                 noe_pairs.append(noe_pair)
     f.close()
 
-    print len(noe_pairs), "NOE pairs",noe_pairs
+    print len(noe_pairs), "CA lines"
 
     return noe_pairs
+
+
+def count_restraints(filename):
+
+    f = open(filename, 'r')
+    noe_pairs = []
+
+    for line in f.readlines():
+            # print line
+            res_a = int(string.split(line)[2])
+            res_b = int(string.split(line)[7])
+
+            name_a = string.split(line)[5].rstrip(")")[:-1]
+            name_b = string.split(line)[10].rstrip(")")[:-1]
+
+            noe_pair = [res_a, res_b, name_a, name_b]
+            if [res_a, res_b, name_a, name_b] not in noe_pairs and \
+               [res_b, res_a, name_b, name_a] not in noe_pairs:
+                noe_pairs.append(noe_pair)
+    f.close()
+
+    print len(noe_pairs), "NOE contacts"
+
+    return len(noe_pairs)
+
 
 
 native_pdb = sys.argv[1]
@@ -30,6 +59,8 @@ noe_file = ""
 if len(sys.argv) == 3:
     noe = True
     noe_file = sys.argv[2]
+
+count_restraints(noe_file)
 
 cmd.load(native_pdb, "native")
 cmd.hide("all")
