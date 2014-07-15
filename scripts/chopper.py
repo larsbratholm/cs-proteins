d96bf44b214e11e99cd47cf9c15989447d740127
Lars Andersen Bratholm <larsbratholm@gmail.com>
2014-07-15 11:45:06 +0200

    added chopper.py to scripts and removed several old scripts

---
+++ scripts/chopper.py
@@ -0,0 +1,29 @@
+import sys
+
+
+input_pdbfiles = []
+
+for arg in sys.argv:
+    if ".pdb" in arg:
+        input_pdbfiles.append(arg)
+
+for input_pdbfile in input_pdbfiles:
+
+    file_lines = open(input_pdbfile, "r").readlines()
+
+    n = 0
+    lines = []
+
+    for line in file_lines:
+        if "MODEL" in line:
+            model_name = input_pdbfile.rstrip(".pdb") + "_%04i.pdb" % (n)
+            lines = []
+        lines.append(line)
+
+        if "ENDMDL" in line:
+            f = open(model_name, "w")
+            for l in lines:
+                f.write(l)
+            f.close()
+            n += 1
+
