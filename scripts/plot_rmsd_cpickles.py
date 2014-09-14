a5dce260c39f3d432a210ed674dbdd3eea5ae54b
Lars <larsbratholm@gmail.com>
2014-09-14 22:29:15 +0200

    updated mbp settings

--- scripts/plot_rmsd_cpickles.py
+++ scripts/plot_rmsd_cpickles.py
@@ -13,7 +13,18 @@ def load_pickle(filename):
 filenames = sys.argv[1:]
 
 for filename in filenames:
+    if "seamless_2.cpickle" in filename or "seamless_wouter_2.cpickle" in filename:
+        continue
     pickle = load_pickle(filename)
+    if "seamless.cpickle" in filename:
+        pickle2 = load_pickle("seamless_2.cpickle")
+        pickle[0] += [x+pickle[0][-1] for x in pickle2[0]]
+        pickle[1] += pickle2[1]
+    if "seamless_wouter.cpickle" in filename:
+        pickle2 = load_pickle("seamless_wouter_2.cpickle")
+        pickle[0] += [x+pickle[0][-1] for x in pickle2[0]]
+        pickle[1] += pickle2[1]
+
     plt.plot(pickle[0],pickle[1],label=filename)
 plt.legend(prop={'size':8})
 plt.savefig("rmsd.png")
