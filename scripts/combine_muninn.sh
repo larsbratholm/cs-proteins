3fe0e154bb8ccf7270fd33b6d8ec6ef38ea726c9
Lars Andersen Bratholm <lab@fend01.cluster>
2014-12-03 16:57:00 +0100

    fixed bugs in new entries

---
+++ scripts/combine_muninn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/phaistos_current/build/bin/muninn/tools/combine_logs -o muninn_combined.txt *.muninn
--- scripts/plot_dat.py
+++ scripts/plot_dat.py
@@ -20,9 +20,9 @@ for line in lines:
         energy = float(string.split(line)[1])
         #energy_1 = float(string.split(line)[2])
         #energy_2 = float(string.split(line)[3])
-        torus  = float(string.split(line)[-1])
-        basilisk  = float(string.split(line)[-3])
-        mumu  = float(string.split(line)[-4])
+        #torus  = float(string.split(line)[-1])
+        #basilisk  = float(string.split(line)[-3])
+        #mumu  = float(string.split(line)[-4])
         #noe_2  = float(string.split(line)[-14])
         #noe_1  = float(string.split(line)[-25])
         rmsd   = float(string.split(line)[-2])
@@ -35,32 +35,32 @@ for line in lines:
         cam = 0#float(string.split(line)[2]) + float(string.split(line)[3])
         ff = energy - cam
         #cam += noe_1
-        if int(sys.argv[1]) == 1:
-            energy = ff+torus+basilisk+cam+mumu*0
-        if int(sys.argv[1]) == 2:
-            energy = ff+torus+basilisk+cam+mumu*1
-        if int(sys.argv[1]) == 3:
-            energy = ff+torus+basilisk+cam+mumu*3
-        if int(sys.argv[1]) == 4:
-            energy = ff+torus+basilisk+cam+mumu*5
-        if int(sys.argv[1]) == 5:
-            energy = ff+cam+mumu*0
-        if int(sys.argv[1]) == 6:
-            energy = ff+cam+mumu*1
-        if int(sys.argv[1]) == 7:
-            energy = ff+cam+mumu*3
-        if int(sys.argv[1]) == 8:
-            energy = ff+cam+mumu*5
-        if int(sys.argv[1]) == 9:
-            energy = torus+basilisk+cam+mumu*0
-        if int(sys.argv[1]) == 10:
-            energy = torus+basilisk+cam+mumu*1
-        if int(sys.argv[1]) == 11:
-            energy = torus+basilisk+cam+mumu*3
-        if int(sys.argv[1]) == 12:
-            energy = torus+basilisk+cam+mumu*5
-        if int(sys.argv[1]) == 13:
-            energy = pp
+        #if int(sys.argv[1]) == 1:
+        #    energy = ff+torus+basilisk+cam+mumu*0
+        #if int(sys.argv[1]) == 2:
+        #    energy = ff+torus+basilisk+cam+mumu*1
+        #if int(sys.argv[1]) == 3:
+        #    energy = ff+torus+basilisk+cam+mumu*3
+        #if int(sys.argv[1]) == 4:
+        #    energy = ff+torus+basilisk+cam+mumu*5
+        #if int(sys.argv[1]) == 5:
+        #    energy = ff+cam+mumu*0
+        #if int(sys.argv[1]) == 6:
+        #    energy = ff+cam+mumu*1
+        #if int(sys.argv[1]) == 7:
+        #    energy = ff+cam+mumu*3
+        #if int(sys.argv[1]) == 8:
+        #    energy = ff+cam+mumu*5
+        #if int(sys.argv[1]) == 9:
+        #    energy = torus+basilisk+cam+mumu*0
+        #if int(sys.argv[1]) == 10:
+        #    energy = torus+basilisk+cam+mumu*1
+        #if int(sys.argv[1]) == 11:
+        #    energy = torus+basilisk+cam+mumu*3
+        #if int(sys.argv[1]) == 12:
+        #    energy = torus+basilisk+cam+mumu*5
+        #if int(sys.argv[1]) == 13:
+        #    energy = pp
         #energy += torus#-float(string.split(line)[4])
 
         if energy < min_energy:
